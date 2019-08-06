2c1959e9a8e8e4a8fef966f9a8067471eda17cf6
Veranicus <polacek.patrik@gmail.com>
2019-08-06 11:50:37 +0200

    closes #3

---
+++ src/main/java/com/patrikpolacek/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,30 @@
+package com.patrikpolacek.msscbeerservice.web.controller;
+
+import com.patrikpolacek.msscbeerservice.web.model.BeerDto;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+//        Todo implement real logic...
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+//        todo implement
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+//    todo implement
+        return new ResponseEntity((HttpStatus.NO_CONTENT));
+    }
+
+}
