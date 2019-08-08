3ae81791deb8df366a9dc217e1ee4f80bcbde878
Veranicus <polacek.patrik@gmail.com>
2019-08-08 16:53:22 +0200

    closes #6

---
+++ src/main/java/com/patrikpolacek/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,13 @@
+package com.patrikpolacek.msscbeerservice.repositories;
+
+import com.patrikpolacek.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+
+}
